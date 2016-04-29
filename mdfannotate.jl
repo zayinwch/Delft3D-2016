ecc65926d7d2a35b1ef05989a272b71ab3b77c10
Rob Campbell <rob10campbell@gmail.com>
2016-04-29 16:30:46 +0900

    updated documentation

--- mdfannotate.jl
+++ mdfannotate.jl
@@ -1,11 +1,14 @@
 #!/usr/bin/env julia
 """
-The function mdfannotate is for correcitng MDF files for Delft3D FLOW: 
-Delft3D FLOW creates MDF files missing "Fmtxxx = #FR#";
+    mdfannotate(inputfile,outputfile)
+
+This function corrects MDF files for Delft3D FLOW: 
+Delft3D FLOW creates MDF files missing lines "Fmtxxx = #FR#";
 this function adds them in the correct places (after lines starting with "Filxxx = ")
 
 By default, the input file is "rawmdf.mdf" and the annotated output file will be saved as "annomdf.mdf")
 """
+
 function mdfannotate(inputfile, outputfile)
     open(inputfile, "r") do inp
         open(outputfile, "w") do otp
