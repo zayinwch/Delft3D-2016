fe164167b2d2260c603948ef29990dac5d694b49
Rob Campbell <rob10campbell@gmail.com>
2016-04-29 15:21:57 +0900

    updated Julia script

--- mdfannotate.jl
+++ mdfannotate.jl
@@ -1,3 +1,21 @@
 """
-This will be a Julia script
+This script is written in Julia
+It annotates MDF files for Delft3D FLOW: 
+Delft3D FLOW creates MDF files missing "Fmtxxx = #FR#";
+this program adds them in the correct places (after lines starting with "Filxxx = ")
+
+By default, the input file is "rawmdf.mdf" and the annotated output file will be saved as "annomdf.mdf")
 """
+
+open("rawmdf.mdf","r") do inp
+         open("annomdf.mdf","w") do otp
+                for ln in eachline(inp)
+                   if ismatch(r"Fil(\w*)",ln)
+                       write(otp,ln)
+                       write(otp,"Fmt",ln[4:6]," = #FR#\n")
+                   else
+                       write(otp,ln)
+                   end
+                end
+        end
+end
